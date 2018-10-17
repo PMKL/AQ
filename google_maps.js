22f9cfa246f52f9b34c32a6b1963a69965e6feca
linh0006 <43328091+linh0006@users.noreply.github.com>
2018-10-17 17:32:15 -0500

    Cleaned Code
    Cleaned up code and removed initialization error by putting google script at bottom of body tag.

--- google_maps.js
+++ google_maps.js
@@ -3,19 +3,10 @@ var blank;
 
 function initMap() {
 	
-  /*var map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 44.977, lng: -93.265},
-    zoom: 6
-  });*/
-  //do you have to call a new map every time?
-  
-   /*var input = document.getElementById('pac-input');
-   var searchBox = new google.maps.places.SearchBox(input);
-   map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);*/
    
    var google_key = "AIzaSyDU2kgfrqoiWwX2WOWBE08KOGz-_nQ5aEY"
   
-	/*var request =  {
+	/*var request =  { //Use this for requesting air quality info
 		url: "https://api.openaq.org/v1/parameters",
 		dataType: "json", 
         success: test
@@ -24,7 +15,7 @@ function initMap() {
 		};
 		$.ajax(request); //Using the ajax method fr*/
 	
-	mapping = new Vue({
+	 mapping = new Vue({
 		el: "#map",
 		data: {
 			center: {lat: 44.977, lng: -93.265},
@@ -46,15 +37,8 @@ function initMap() {
       data: {
 			counter: 0,
 			search_request: '',
-			search_results: []
+			//search_results: []
         }
-	  /*methods: {
-			cityRequest: function (event) {
-				//console.log(event.target.innerHTML);
-				console.log(this.search_request)
-			}
-			
-	  },*/
 		
         
     })
@@ -62,18 +46,12 @@ function initMap() {
 	
 	mapping.createMap();
 	
-	
-
-	//var removeItem = document.getElementById("search");
-	
-	//removeItem.addEventListener("click", clicked, false);
 }
 
 function google_Search(event){
 	
 	var request2 = {
 		url:"https://maps.googleapis.com/maps/api/geocode/json?address=" + blank.search_request + "&key=AIzaSyCsz_H86EN2uQh7eB0aNUqP0WDf3lIU2ZA",
-		//url:"https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=AIzaSyCsz_H86EN2uQh7eB0aNUqP0WDf3lIU2ZA",
 		dataType: "json",
 		success: googleSuccess
 	};
@@ -89,14 +67,7 @@ function googleSuccess(data){
 	mapping.center.lng = parseFloat(data.results[0].geometry.location.lng)
 	
 	mapping.createMap();
-	//map = new google.maps.Map(document.getElementById('map'), {
-    //center: {lat: 100.977, lng: -93.265}, //{lat: parseFloat(data.results[0].geometry.location[0]), lng: parseFloat(data.results[0].geometry.location[1])},
-    //zoom: 6
-  //});
+	
 }
 	
-function test(data){
-	console.log(data);
-}
-
 
